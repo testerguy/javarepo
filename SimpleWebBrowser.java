033359289b0fd6f3c74b250e9df94af3a5877746
Testerguy <testerguy@users.noreply.github.com>
2015-05-21 23:33:28 -0700

    commit

---
+++ SimpleWebBrowser.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class SimpleWebBrowser extends JFrame {
+	JTextField addressBar;
+	JEditorPane display;
+	
+	public SimpleWebBrowser() {
+		
+	}
+}
