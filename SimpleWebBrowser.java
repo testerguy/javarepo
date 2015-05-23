a166bcf84559b86e4be88ec3cecab00f763c93e7
Testerguy <testerguy@users.noreply.github.com>
2015-05-23 00:51:25 -0700

    finish browser

--- SimpleWebBrowser.java
+++ SimpleWebBrowser.java
@@ -8,6 +8,43 @@ public class SimpleWebBrowser extends JFrame {
 	JEditorPane display;
 	
 	public SimpleWebBrowser() {
+		addressBar = new JTextField("enter URL!");
+		display = new JEditorPane();
 		
+		addressBar.addActionListener(
+				new ActionListener() {
+					public void actionPerformed (ActionEvent event) {
+						// pass content of JTextField to loadData()
+						loadData(event.getActionCommand());
+					}
+				}
+		);
+		
+		add(addressBar, BorderLayout.NORTH);
+		
+		display.addHyperlinkListener(
+				new HyperlinkListener() {
+					public void hyperlinkUpdate(HyperlinkEvent event) {
+						// if hyperlink ACTIVATED, pass it to loadData() and set 'addressBar' to it, both as String
+						if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) 
+							loadData(event.getURL().toString());
+					}
+				}
+		);
+		
+		add(new JScrollPane(display), BorderLayout.CENTER);
+		
+		setVisible(true);
+		setSize(600,600);
+	}
+	
+	private void loadData(String s) {
+		// set 'display' to url in string
+		try {
+			display.setPage(s);
+			addressBar.setText(s);
+		} catch (Exception e) {
+			System.out.println("not valid URL!");
+		}
 	}
 }
---
+++ SimpleWebBrowser_Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class SimpleWebBrowser_Main {
+	public static void main(String[] args) {
+		SimpleWebBrowser browser = new SimpleWebBrowser();
+		
+		browser.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
